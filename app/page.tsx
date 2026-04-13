907965062bce2e6897a42a4f02a175947354e786
yasseraissou300-arch <yasseraissou300@gmail.com>
2026-04-13 22:49:06 +0100

    Update Home function to handle user authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { auth } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
-export default function Home() {
-  redirect('/login')
+export default async function Home() {
+    const session = await auth()
+    if (session?.user) {
+          redirect('/commandes')
+    }
+    redirect('/login')
 }
